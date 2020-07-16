72585678340d2a8d07903effa0107d3cd21b301a
gitRepositry <saleh_hamkar@yahoo.com>
2020-07-15 21:06:09 -0400

    changes were made by me

---
+++ GitExcersise/src/main/java/local/packag/HelloGitHub.java
@@ -0,0 +1,11 @@
+package local.packag;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the practice part to push");
+
+	}
+
+}
